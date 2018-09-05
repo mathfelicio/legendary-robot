7324875404ea3ff0374dec236d5bddbb98522562
Matheus Felicio <matheusf@qualitysistemas.com.br>
2018-09-05 17:18:56 -0400

    Correção na estrutura de arquivo

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo 'Teste PHP <br><br>';
-
-echo 'Dados acesso banco Mysql<br>';
-echo 'host: localhost <br>';
-echo 'user: root <br>';
-echo 'database: teste_php <br>';
-echo 'senha: root <br>';
